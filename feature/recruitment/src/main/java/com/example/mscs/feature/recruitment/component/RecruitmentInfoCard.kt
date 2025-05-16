db802aa43aee0dc56df8daeaf9a4ab65c7009a12
yongho2157 <studyyongho2157@gmail.com>
2025-05-16 20:35:07 +0900

    refactor: 채용 카드 containerColor 변경

--- feature/recruitment/src/main/java/com/example/mscs/feature/recruitment/component/RecruitmentInfoCard.kt
+++ feature/recruitment/src/main/java/com/example/mscs/feature/recruitment/component/RecruitmentInfoCard.kt
@@ -7,7 +7,6 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
@@ -23,6 +22,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
@@ -42,7 +42,7 @@ fun RecruitmentInfoCard(
         elevation = CardDefaults.cardElevation(defaultElevation = 2.dp),
         shape = RoundedCornerShape(12.dp),
         colors = CardDefaults.cardColors(
-            containerColor = MaterialTheme.colorScheme.surface
+            containerColor = Color.White
         ),
         modifier = modifier
             .fillMaxWidth()
